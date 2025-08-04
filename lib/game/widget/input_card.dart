b864edd7f5db2318cc5f72bd021cb86efaad0a0a
Ha Da Hyun <https://github.com/dahyun27>
2025-08-04 14:56:55 +0900

    InputCard 컴포넌트 및 탭 반응 InkWell 적용

--- lib/game/widget/input_card.dart
+++ lib/game/widget/input_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class InputCard extends StatelessWidget {
+  final VoidCallback? callback;
   final Widget child;
 
   const InputCard({
+    this.callback,
     required this.child,
     super.key,
   });
@@ -11,9 +13,29 @@ class InputCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        child: child,
+      child: InkWell(onTap: () => callback?.call(), child: InputContents(child: child)),
+    );
+  }
+}
+
+class InputContents extends StatelessWidget {
+  const InputContents({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        border: Border.all(color: Colors.grey, width: 6)
       ),
+      child: child,
     );
   }
 }
